fccf47b2130fcbaed22071aec65beee7f1e06502
Ben Longhurst <benjalon@gmail.com>
2019-01-24 18:42:53 +0000

    Started importing/writing shaders.

--- GamesLabCW/GamesLabCW.cpp
+++ GamesLabCW/GamesLabCW.cpp
@@ -1,6 +1,17 @@
+#include <stdio.h>
+#include <string>
+using namespace std;
+#include <GL/glew.h>
 #include <GL/freeglut.h>
+#include <glm\glm.hpp>
+using namespace glm;
 
 #include "GamesLabCW.h"
+
+GLuint VBO;
+
+const char* pVSFileName = "shader.vs";
+const char* pFSFileName = "shader.fs";
 
 int main(int argc, char** argv)
 {
@@ -9,13 +20,27 @@ int main(int argc, char** argv)
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowSize(1024, 768);
 	glutInitWindowPosition(100, 100);
-	glutCreateWindow("Tutorial 01");
+	glutCreateWindow("RBS");
 
 	// Pass buffer clear function
 	glutDisplayFunc(RenderSceneCB);
 
+	// Initialize GLEW
+	GLenum res = glewInit();
+	if (res != GLEW_OK) {
+		fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+		return 1;
+	}
+
+	printf("GL version: %s\n", glGetString(GL_VERSION));
+
 	// Render objects
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+	CreateVertexBuffer();
+
+	CompileShaders();
+
 	glutMainLoop();
 
 	return 0;
@@ -27,5 +52,97 @@ int main(int argc, char** argv)
 static void RenderSceneCB()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
+
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+	glDisableVertexAttribArray(0);
+
 	glutSwapBuffers();
+}
+
+static void CreateVertexBuffer()
+{
+	vec3 Vertices[3];
+	Vertices[0] = vec3(-1.0f, -1.0f, 0.0f);
+	Vertices[1] = vec3(1.0f, -1.0f, 0.0f);
+	Vertices[2] = vec3(0.0f, 1.0f, 0.0f);
+
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+}
+
+static void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType)
+{
+	GLuint ShaderObj = glCreateShader(ShaderType);
+
+	if (ShaderObj == 0) {
+		fprintf(stderr, "Error creating shader type %d\n", ShaderType);
+		exit(0);
+	}
+
+	const GLchar* p[1];
+	p[0] = pShaderText;
+	GLint Lengths[1];
+	Lengths[0] = strlen(pShaderText);
+	glShaderSource(ShaderObj, 1, p, Lengths);
+	glCompileShader(ShaderObj);
+	GLint success;
+	glGetShaderiv(ShaderObj, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		GLchar InfoLog[1024];
+		glGetShaderInfoLog(ShaderObj, 1024, NULL, InfoLog);
+		fprintf(stderr, "Error compiling shader type %d: '%s'\n", ShaderType, InfoLog);
+		exit(1);
+	}
+
+	glAttachShader(ShaderProgram, ShaderObj);
+}
+
+static void CompileShaders()
+{
+	GLuint ShaderProgram = glCreateProgram();
+
+	if (ShaderProgram == 0) {
+		fprintf(stderr, "Error creating shader program\n");
+		exit(1);
+	}
+
+	string vs, fs;
+
+	if (!ReadFile(pVSFileName, vs)) {
+		exit(1);
+	};
+
+	if (!ReadFile(pFSFileName, fs)) {
+		exit(1);
+	};
+
+	AddShader(ShaderProgram, vs.c_str(), GL_VERTEX_SHADER);
+	AddShader(ShaderProgram, fs.c_str(), GL_FRAGMENT_SHADER);
+
+	GLint Success = 0;
+	GLchar ErrorLog[1024] = { 0 };
+
+	glLinkProgram(ShaderProgram);
+	glGetProgramiv(ShaderProgram, GL_LINK_STATUS, &Success);
+	if (Success == 0) {
+		glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
+		fprintf(stderr, "Error linking shader program: '%s'\n", ErrorLog);
+		exit(1);
+	}
+
+	glValidateProgram(ShaderProgram);
+	glGetProgramiv(ShaderProgram, GL_VALIDATE_STATUS, &Success);
+	if (!Success) {
+		glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
+		fprintf(stderr, "Invalid shader program: '%s'\n", ErrorLog);
+		exit(1);
+	}
+
+	glUseProgram(ShaderProgram);
 }
--- GamesLabCW/GamesLabCW.h
+++ GamesLabCW/GamesLabCW.h
@@ -1,1 +1,4 @@
 static void RenderSceneCB();
+static void CreateVertexBuffer();
+static void CreateVertexBuffer();
+static void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType);
