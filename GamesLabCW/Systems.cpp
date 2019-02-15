740f7f22e7458c6044dbb0111f7f680ff3aacfb4
Ben Longhurst <benjalon@gmail.com>
2019-02-15 15:54:49 +0000

    Added ghost camera control

--- GamesLabCW/Systems.cpp
+++ GamesLabCW/Systems.cpp
@@ -28,4 +28,23 @@ namespace game::systems
 		std::cout << n.name << std::endl;
 	};
 	SYSTEM(NameSystem, NameComponent);
+
+	//EXAMPLE System that uses input to turn lights on if mouse is held
+	auto LightMouseSystem = [](double dt, auto entity, auto &al)
+	{
+		al.intensity = utility::contains(input::held, input::MOUSE_BUTTON_LEFT) ? 1.0 : 0.0;
+	};
+	SYSTEM(LightMouseSystem, AmbientLightComponent);
+
+	auto MoveCameraSystem = [](double dt, auto entity, CameraComponent &c)
+	{
+		auto speed = 0.3;
+		c.position.x += utility::contains(input::held, input::KEY_A) ? -speed : 0.0;
+		c.position.x += utility::contains(input::held, input::KEY_D) ? speed: 0.0;
+		c.position.z += utility::contains(input::held, input::KEY_S) ? speed : 0.0;
+		c.position.z += utility::contains(input::held, input::KEY_W) ? -speed : 0.0;
+		c.orientation.x += utility::contains(input::held, input::KEY_Q) ? speed*2 : 0.0;
+		c.orientation.x += utility::contains(input::held, input::KEY_E) ? -speed*2 : 0.0;
+	};
+	SYSTEM(MoveCameraSystem, CameraComponent);
 }
