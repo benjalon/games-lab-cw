0abbe4dd763305528202057b7978c1768e0d5b88
Ben Longhurst <benjalon@gmail.com>
2019-03-01 15:02:40 +0000

    made moving camera

--- GamesLabCW/Systems.cpp
+++ GamesLabCW/Systems.cpp
@@ -25,9 +25,39 @@ namespace game::systems
 	{
 		double linear = 18.0 * info.dt;
 		double angular = 36.0 * info.dt;
-		c.position.x += (input::is_held(input::KEY_D) - input::is_held(input::KEY_A)) * linear;
-		c.position.z += (input::is_held(input::KEY_S) - input::is_held(input::KEY_W)) * linear;
-		c.orientation.x += (input::is_held(input::KEY_Q) - input::is_held(input::KEY_E)) * angular;
+		
+		double xpos, ypos;
+		float horizontalAngle = 3.14f;
+		// vertical angle : 0, look at the horizon
+		float verticalAngle = 0.0f;
+		float speed = 3.0f; // 3 units / second
+		float mouseSpeed = 0.1f;
+
+		xpos = input::cursor_pos.x;
+		ypos = input::cursor_pos.y;
+
+		c.orientation.x += mouseSpeed * info.dt * float(1920 / 2 - xpos);
+		c.orientation.y += mouseSpeed * info.dt * float(1080 / 2 - ypos);
+
+		input::cursor_pos = { 1920 / 2, 1080 / 2 };
+
+		if (utility::contains(input::held, input::KEY_W))
+		{
+			c.position += c.orientation.direction_hv() *info.dt * speed;
+		}
+		if (utility::contains(input::held, input::KEY_S))
+		{
+			c.position -= c.orientation.direction_hv() * info.dt * speed;
+		}
+		if (utility::contains(input::held, input::KEY_D))
+		{
+			c.position += c.orientation.direction_hv_right() *info.dt * speed;
+		}
+		if (utility::contains(input::held, input::KEY_A))
+		{
+			c.position -= c.orientation.direction_hv_right() * info.dt * speed;
+		}
+
 	};
 	SYSTEM(MoveCameraSystem, CameraComponent);
 }
