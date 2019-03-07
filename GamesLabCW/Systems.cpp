e7c896ee75e02b173db58c252e57cf24fe1ba2af
Ben Longhurst <benjalon@gmail.com>
2019-03-07 12:55:57 +0000

    made camera more fluid and fixed bugs.

--- GamesLabCW/Systems.cpp
+++ GamesLabCW/Systems.cpp
@@ -23,28 +23,29 @@ namespace game::systems
 	//Allows for noclip camera control by the player
 	auto MoveCameraSystem = [](SceneInfo info, auto entity, CameraComponent &c)
 	{
-		//ShowCursor(FALSE);
-		SetCursorPos(1920 / 2, 1080 / 2);
-
-		double linear = 18.0 * info.dt;
-		double angular = 36.0 * info.dt;
+		ShowCursor(FALSE);
+		double screenWidth = 1920;
+		double screenHeight = 1080;
+		if (!utility::contains(input::held, input::KEY_ESCAPE))
+		{
+			//This has been hacked because of no fullscreen meaning it needs an offset.
+			SetCursorPos(1935 / 2, 1145 / 2);
+		}
 		
 		double xpos, ypos;
-		float horizontalAngle = 3.14f;
-		// vertical angle : 0, look at the horizon
-		float verticalAngle = 0.0f;
-		float speed = 3.0f; // 3 units / second
-		float mouseSpeed = 0.4f;
+		float speed = 11.0f; // 11 units / second
+		float mouseSpeedx = 2.0f;
+		float mouseSpeedy = 2.5f;
 
 		xpos = input::cursor_pos.x;
 		ypos = input::cursor_pos.y;
 
-		c.orientation.x += mouseSpeed * info.dt * float(1920 / 2 - xpos);
-		c.orientation.y += mouseSpeed * info.dt * float(1080 / 2 - ypos);
+		c.orientation.x += mouseSpeedx * info.dt * float(screenWidth / 2 - xpos);
+		c.orientation.y += mouseSpeedy * info.dt * float(screenHeight / 2 - ypos);
 
 		if (utility::contains(input::held, input::KEY_W))
 		{
-			c.position += c.orientation.direction_hv() *info.dt * speed;
+			c.position += c.orientation.direction_hv() * info.dt * speed;
 		}
 		if (utility::contains(input::held, input::KEY_S))
 		{
@@ -59,7 +60,7 @@ namespace game::systems
 			c.position -= c.orientation.direction_hv_right() * info.dt * speed;
 		}
 
-		input::cursor_pos = { 1920 / 2, 1080 / 2 };
+		input::cursor_pos = { screenWidth / 2, screenHeight / 2 };
 	};
 	SYSTEM(MoveCameraSystem, CameraComponent);
 }
