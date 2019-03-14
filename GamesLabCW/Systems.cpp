7ae01f5f612e532fd45ce1f3a42ca08a68e5f061
Sam Griffiths <13700330+sjgriffiths@users.noreply.github.com>
2019-03-14 00:35:59 +0000

    Re-added jump simulation in absence of collisions

--- GamesLabCW/Systems.cpp
+++ GamesLabCW/Systems.cpp
@@ -22,15 +22,6 @@ namespace game::systems
 	SYSTEM(GameStateSystem, GameStateComponent);
 
 
-	//Basic kinematic system of calculus of motion
-	auto KinematicSystem = [](auto info, auto entity, auto &t, auto &k)
-	{
-		k.velocity += k.acceleration * info.dt;
-		t.position += k.velocity * info.dt;
-	};
-	SYSTEM(KinematicSystem, TransformComponent, KinematicComponent);
-
-
 	//First-person control by the player
 	auto FirstPersonControllerSystem = [](SceneInfo info, auto entity, FirstPersonControllerComponent &f, TransformComponent &t, KinematicComponent &k)
 	{
@@ -45,14 +36,36 @@ namespace game::systems
 		t.rotation.y = std::clamp(t.rotation.y, -60.0, 60.0);
 
 		//Move forward/backward
-		t.position += Vector2(t.rotation.x, t.rotation.y).direction_hv() * move_speed * info.dt *
+		k.velocity = Vector2(t.rotation.x, t.rotation.y).direction_hv() * move_speed *
 			(input::is_held(input::KEY_W) - input::is_held(input::KEY_S));
 
 		//Strafe
-		t.position += Vector2(t.rotation.x, t.rotation.y).direction_hv_right() * move_speed * info.dt *
+		k.velocity += Vector2(t.rotation.x, t.rotation.y).direction_hv_right() * move_speed *
 			(input::is_held(input::KEY_D) - input::is_held(input::KEY_A));
+
+
+		/* JUMPING SIMULATION IN ABSENCE OF COLLISIONS */
+		////Acceleration due to gravity
+		//k.acceleration.y = -9.8;
+		////Clamp to non-neg y pos
+		//t.position.y = std::max(t.position.y, 0.0);
+		////Simulate solidness
+		//if (t.position.y == 0.0)
+		//	k.velocity.y = std::max(k.velocity.y, 0.0);
+		////Jump!
+		//if (input::is_pressed(input::KEY_SPACE))
+		//	k.velocity.y += 4.0;
 	};
 	SYSTEM(FirstPersonControllerSystem, FirstPersonControllerComponent, TransformComponent, KinematicComponent);
+
+
+	//Basic kinematic system of calculus of motion
+	auto KinematicSystem = [](auto info, auto entity, auto &t, auto &k)
+	{
+		k.velocity += k.acceleration * info.dt;
+		t.position += k.velocity * info.dt;
+	};
+	SYSTEM(KinematicSystem, TransformComponent, KinematicComponent);
 
 
 	//Makes a camera follow its target
@@ -61,7 +74,6 @@ namespace game::systems
 		TransformComponent &t = info.scene.get<TransformComponent>(c.follow);
 		c.position = t.position;
 		c.orientation = { t.rotation.x, t.rotation.y };
-		std::cout << c.orientation.y << std::endl;
 	};
 	SYSTEM(MoveCameraSystem, CameraComponent);
 }
