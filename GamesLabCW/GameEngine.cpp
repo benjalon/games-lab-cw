468c930b05d397c3e2d020f0d16156b1cc2a2660
Ryan <mail@ryanjp.co.uk>
2019-05-20 17:39:56 +0100

    Cleaned up GameEngine a bit.

--- GamesLabCW/GameEngine.cpp
+++ GamesLabCW/GameEngine.cpp
@@ -59,7 +59,7 @@ game::GameEngine::GameEngine(bool fullscreen, bool vsync, bool ground) :
 
 	//EXAMPLE Load models
 	renderer::load_model("models/Cyborg/cyborg.obj");
-    renderer::load_model("models/Room/room.obj");
+	renderer::load_model("models/Room/room.obj");
 	renderer::load_model("models/Procedural/type1.obj");
 	renderer::load_model("models/Procedural/type2.obj");
 	renderer::load_model("models/Procedural/type3.obj");
@@ -90,11 +90,11 @@ game::GameEngine::GameEngine(bool fullscreen, bool vsync, bool ground) :
 
 void game::GameEngine::run()
 {
-	//EXAMPLE Instantiate a camera and models
+	// Player/camera
 	auto player = scene_.instantiate("FirstPersonController", TransformComponent{ {0,6,5} , { 180,0,0 } });
 	auto camera = CameraComponent{ player };
-	//scene_.instantiate("Camera", camera);
 
+	// Room stuff
 	ModelComponent m_water; m_water.model_file = "models/Water/water.obj"; m_water.vertex_shader = "shaders/Water.vert"; m_water.fragment_shader = "shaders/Water.frag";
 	scene_.instantiate("Model", m_water);
 
@@ -102,127 +102,91 @@ void game::GameEngine::run()
 	TransformComponent t_room; t_room.position.y = 10; t_room.scale = { 0.5, 0.5, 0.5 };
 	scene_.instantiate("Model", m_room, t_room);
 
-	/*ParticleComponent p_test; p_test.texture_file = "models/particle.png"; p_test.respawn_count = 2;
-	scene_.instantiate("ParticleEffect", p_test);*/
-
-	// Procedural stuff
-	/*ModelComponent m_type1; m_type1.model_file = "models/Procedural/type1.obj";
-	scene_.instantiate("Model", m_type1);
-
-	ModelComponent m_type2; m_type2.model_file = "models/Procedural/type2.obj";
-	scene_.instantiate("Model", m_type2);
-
-	ModelComponent m_type3; m_type3.model_file = "models/Procedural/type3.obj";
-	scene_.instantiate("Model", m_type3);
-
-	ModelComponent m_type4; m_type4.model_file = "models/Procedural/type4.obj";
-	scene_.instantiate("Model", m_type4);*/
-
 	// Keys
+	ModelComponent m_key; m_key.model_file = "models/Key/Key_B_02.obj";
+
 	KeyComponent k_key1; k_key1.destination = { -40, 10, 10 };
-	ModelComponent m_key1; m_key1.model_file = "models/Key/Key_B_02.obj";
 	TransformComponent t_key1; t_key1.scale = { 0.5, 0.5, 0.5 }; t_key1.position = { 0, 0.5, 15 }; t_key1.rotation = { 90, 180, 180 };
 	PointLightComponent pl_key1{ {1, 180 / 255.0, 120.0 / 255.0}, 0.5, t_key1.position };
-	scene_.instantiate("Key", m_key1, t_key1, k_key1, pl_key1);
+	scene_.instantiate("Key", m_key, t_key1, k_key1, pl_key1);
 
 	KeyComponent k_key2; k_key2.destination = { -40, 15, 10 };
-	ModelComponent m_key2; m_key2.model_file = "models/Key/Key_B_02.obj";
 	TransformComponent t_key2; t_key2.scale = { 0.5, 0.5, 0.5 }; t_key2.position = { 0, 0.5, -15 }; t_key2.rotation = { 90, 180, 180 };
 	PointLightComponent pl_key2{ {1, 180 / 255.0, 120.0 / 255.0}, 0.5, t_key2.position };
-	scene_.instantiate("Key", m_key2, t_key2, k_key2, pl_key2);
+	scene_.instantiate("Key", m_key, t_key2, k_key2, pl_key2);
 
 	KeyComponent k_key3; k_key3.destination = { -40, 20, 10 };
-	ModelComponent m_key3; m_key3.model_file = "models/Key/Key_B_02.obj";
 	TransformComponent t_key3; t_key3.scale = { 0.5, 0.5, 0.5 }; t_key3.position = { 10, 0.5, 15 }; t_key3.rotation = { 90, 180, 180 };
 	PointLightComponent pl_key3{ {1, 180 / 255.0, 120.0 / 255.0}, 0.5, t_key3.position };
-	scene_.instantiate("Key", m_key3, t_key3, k_key3, pl_key3);
+	scene_.instantiate("Key", m_key, t_key3, k_key3, pl_key3);
 
 	KeyComponent k_key4; k_key4.destination = { -40, 10, -10 };
-	ModelComponent m_key4; m_key4.model_file = "models/Key/Key_B_02.obj";
 	TransformComponent t_key4; t_key4.scale = { 0.5, 0.5, 0.5 }; t_key4.position = { -10, 0.5, 15 }; t_key4.rotation = { 90, 180, 180 };
 	PointLightComponent pl_key4{ {1, 180 / 255.0, 120.0 / 255.0}, 0.5, t_key4.position };
-	scene_.instantiate("Key", m_key4, t_key4, k_key4, pl_key4);
+	scene_.instantiate("Key", m_key, t_key4, k_key4, pl_key4);
 
 	KeyComponent k_key5; k_key5.destination = { -40, 15, -10 };
-	ModelComponent m_key5; m_key5.model_file = "models/Key/Key_B_02.obj";
 	TransformComponent t_key5; t_key5.scale = { 0.5, 0.5, 0.5 }; t_key5.position = { 30, 0.5, 10 }; t_key5.rotation = { 90, 180, 180 };
 	PointLightComponent pl_key5{ {1, 180 / 255.0, 120.0 / 255.0}, 0.5, t_key5.position };
-	scene_.instantiate("Key", m_key5, t_key5, k_key5, pl_key5);
+	scene_.instantiate("Key", m_key, t_key5, k_key5, pl_key5);
 
 	KeyComponent k_key6; k_key6.destination = { -40, 20, -10 };
-	ModelComponent m_key6; m_key6.model_file = "models/Key/Key_B_02.obj";
 	TransformComponent t_key6; t_key6.scale = { 0.5, 0.5, 0.5 }; t_key6.position = { -30, 0.5, 10 }; t_key6.rotation = { 90, 180, 180 };
 	PointLightComponent pl_key6{ {1, 180 / 255.0, 120.0 / 255.0}, 0.5, t_key6.position };
-	scene_.instantiate("Key", m_key6, t_key6, k_key6, pl_key6);
+	scene_.instantiate("Key", m_key, t_key6, k_key6, pl_key6);
 
+	// Minotaur test model
 	ModelComponent m_minotaur; m_minotaur.model_file = "models/Minotaur/Minotaur@Jump.fbx";
 	ColourComponent c_minotaur; c_minotaur.colour = { 0, 0, 255 };
 	TransformComponent t_minotaur; t_minotaur.scale = { 0.15, 0.15, 0.15 }; t_minotaur.position = { 0, 9, -15 }; t_minotaur.rotation = { 90, 180, 0 };//90, 180
 	DetectionComponent d_minotaur; d_minotaur.c.radius = 10;
 	scene_.instantiate("AIModel", m_minotaur, t_minotaur, camera, d_minotaur, c_minotaur);
 
-	ModelComponent m_torch1; m_torch1.model_file = "models/Torch/torch.obj";
+	// Torches
+	ModelComponent m_torch; m_torch.model_file = "models/Torch/torch.obj";
+
+	ParticleComponent p_torch; p_torch.texture_file = "models/fire.png"; p_torch.respawn_count = 1;
+	p_torch.position_variation = Vector3(100, 50, 40);
+	p_torch.velocity_variation = Vector3(100, 50, 50);
+	p_torch.color_variation = Vector3(100, -0.5, 100);
+	p_torch.color_modifier = Vector3(1, 0.15, 0);
+	p_torch.scale_variation = Vector3(100, 50, 10);
+
 	TransformComponent t_torch1; t_torch1.position = { 26, 0, -23 }; t_torch1.scale = { 5, 5, 5 };
-	scene_.instantiate("Model", m_torch1, t_torch1);
+	scene_.instantiate("Model", m_torch, t_torch1);
 	scene_.instantiate("PointLight", PointLightComponent{ {1, 147.0 / 255.0, 41.0 / 255.0}, 40, {26, 7, -23} });
-	ParticleComponent p_torch1; p_torch1.texture_file = "models/fire.png"; p_torch1.respawn_count = 1;
-	p_torch1.position_variation = Vector3(100, 50, 40);
-	p_torch1.velocity_variation = Vector3(100, 50, 50);
-	p_torch1.color_variation = Vector3(100, -0.5, 100);
-	p_torch1.color_modifier = Vector3(1, 0.15, 0);
-	p_torch1.scale_variation = Vector3(100, 50, 10);
-	scene_.instantiate("ParticleEffect", p_torch1, TransformComponent{ { 26, 8, -23 } });
+	scene_.instantiate("ParticleEffect", p_torch, TransformComponent{ { 26, 8, -23 } });
 
-	ModelComponent m_torch2; m_torch2.model_file = "models/Torch/torch.obj";
 	TransformComponent t_torch2; t_torch2.position = { 26, 0, 23 }; t_torch2.scale = { 5, 5, 5 };
-	scene_.instantiate("Model", m_torch2, t_torch2);
+	scene_.instantiate("Model", m_torch, t_torch2);
 	scene_.instantiate("PointLight", PointLightComponent{ {1, 147.0 / 255.0, 41.0 / 255.0}, 40, {26, 7, 23} });
-	ParticleComponent p_torch2; p_torch2.texture_file = "models/fire.png"; p_torch2.respawn_count = 1;
-	p_torch2.position_variation = Vector3(100, 50, 40);
-	p_torch2.velocity_variation = Vector3(100, 50, 50);
-	p_torch2.color_variation = Vector3(100, -0.5, 100);
-	p_torch2.color_modifier = Vector3(1, 0.15, 0);
-	p_torch2.scale_variation = Vector3(100, 50, 10);
-	scene_.instantiate("ParticleEffect", p_torch2, TransformComponent{ { 26, 8, 23 } });
+	scene_.instantiate("ParticleEffect", p_torch, TransformComponent{ { 26, 8, 23 } });
 
-	ModelComponent m_torch3; m_torch3.model_file = "models/Torch/torch.obj";
 	TransformComponent t_torch3; t_torch3.position = { -26, 6, -10 }; t_torch3.scale = { 5, 5, 5 };
-	scene_.instantiate("Model", m_torch3, t_torch3);
+	scene_.instantiate("Model", m_torch, t_torch3);
 	scene_.instantiate("PointLight", PointLightComponent{ {1, 147.0 / 255.0, 41.0 / 255.0}, 40, { -26, 13, -10 } });
-	ParticleComponent p_torch3; p_torch3.texture_file = "models/fire.png"; p_torch3.respawn_count = 1;
-	p_torch3.position_variation = Vector3(100, 50, 40);
-	p_torch3.velocity_variation = Vector3(100, 50, 50);
-	p_torch3.color_variation = Vector3(100, -0.5, 100);
-	p_torch3.color_modifier = Vector3(1, 0.15, 0);
-	p_torch3.scale_variation = Vector3(100, 50, 10);
-	scene_.instantiate("ParticleEffect", p_torch3, TransformComponent{ { -26, 14, -10 } });
+	scene_.instantiate("ParticleEffect", p_torch, TransformComponent{ { -26, 14, -10 } });
 
-	ModelComponent m_torch4; m_torch4.model_file = "models/Torch/torch.obj";
 	TransformComponent t_torch4; t_torch4.position = { -26, 6, 10 }; t_torch4.scale = { 5, 5, 5 };
-	scene_.instantiate("Model", m_torch4, t_torch4);
+	scene_.instantiate("Model", m_torch, t_torch4);
 	scene_.instantiate("PointLight", PointLightComponent{ {1, 147.0 / 255.0, 41.0 / 255.0}, 40, { -26, 13, 10 } });
-	ParticleComponent p_torch4; p_torch4.texture_file = "models/fire.png"; p_torch4.respawn_count = 1;
-	p_torch4.position_variation = Vector3(100, 50, 40);
-	p_torch4.velocity_variation = Vector3(100, 50, 50);
-	p_torch4.color_variation = Vector3(100, -0.5, 100);
-	p_torch4.color_modifier = Vector3(1, 0.15, 0);
-	p_torch4.scale_variation = Vector3(100, 50, 10);
-	scene_.instantiate("ParticleEffect", p_torch4, TransformComponent{ { -26, 14, 10 } });
+	scene_.instantiate("ParticleEffect", p_torch, TransformComponent{ { -26, 14, 10 } });
 
-	// Portal light
-	scene_.instantiate("PointLight", PointLightComponent{ {1, 105.0 / 255.0, 180.0 / 255.0}, 40, { 3, 3, 22} });
-
+	// Portal
 	ParticleComponent p_portal; p_portal.texture_file = "models/star.png"; p_portal.respawn_count = 1;
 	p_portal.position_variation = Vector3(100, 50, 10);
 	p_portal.velocity_variation = Vector3(100, 50, 40);
 	p_portal.color_variation = Vector3(100, -0.5, 100);
 	p_portal.color_modifier = Vector3(0.8, 0.2, 1);
 	scene_.instantiate("ParticleEffect", p_portal, TransformComponent{ Vector3(3, 5, 22) });
+	scene_.instantiate("PointLight", PointLightComponent{ {1, 105.0 / 255.0, 180.0 / 255.0}, 40, { 3, 3, 22} });
 
+	// Skybox
 	TransformComponent t_skybox; t_skybox.scale = { 20, 20, 20 };
 	ModelComponent m_skybox; m_skybox.model_file = "models/Skybox/skybox.obj"; m_skybox.vertex_shader = "shaders/Skybox.vert"; m_skybox.fragment_shader = "shaders/Skybox.frag";
 	scene_.instantiate("Model", m_skybox, t_skybox);
 
+	// Generic scene lighting
 	scene_.instantiate("AmbientLight", AmbientLightComponent{ {1, 147.0 / 255.0, 41.0 / 255.0}, 0.01 });
 	scene_.instantiate("DirectionalLight", DirectionalLightComponent{ {0, 0, 0}, 0, {0,0,0} });
 	/*scene_.instantiate("PointLight", PointLightComponent{ {1, 147.0 / 255.0, 41.0 / 255.0}, 1, {0,5,0} });*/
